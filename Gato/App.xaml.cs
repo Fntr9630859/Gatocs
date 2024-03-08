844dcb2c18cea9871b1133e425de61bdaa222158
FNTR3455234 <felipeant06@gmail.com>
2024-03-07 22:56:49 -0600

    Agregar archivos de proyecto.

---
+++ Gato/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gato
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
